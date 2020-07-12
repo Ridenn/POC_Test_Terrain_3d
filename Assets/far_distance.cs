e0efa2dc8ee9c31e9e1e38b259b3614bc67c4841
Lucas <lucaspedrosa11@hotmail.com>
2020-07-12 20:11:59 -0300

    Created test terrain with light effects

---
+++ Assets/far_distance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class far_distance : MonoBehaviour
+{
+    public float distance;
+    public Terrain terrain;
+    void Start()
+    {
+        terrain.treeDistance = distance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
